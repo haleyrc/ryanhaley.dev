748bb22260b4f409283c77641711c0a9aa3cd6ff
Ryan Haley <ryanhaley@frazer.biz>
2019-04-18 21:14:38 -0400

    Setup source filesystem and remark

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,26 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: "Gridsome",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "src/posts/**/*.md",
+        typeName: "Post",
+        route: "/posts/:year/:month/:day/:slug"
+      }
+    }
+  ],
+
+  transformers: {
+    remark: {
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+    }
+  }
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,6 +10,14 @@
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>
 
+    <ul>
+        <li v-for="edge in $static.allPost.edges" :key="edge.id">
+            <g-link :to="`posts/${edge.node.year}/${edge.node.month}/${edge.node.day}/${edge.node.slug}`">
+                {{edge.node.title}}
+            </g-link>
+        </li>
+    </ul>
+    
     <p class="home-links">
       <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
@@ -18,12 +26,31 @@
   </Layout>
 </template>
 
+<static-query>
+query {
+  allPost {
+    edges {
+        node {
+            id
+            title
+            excerpt
+            slug 
+            year:date (format:"YYYY")
+            month:date (format:"MM")
+            day:date(format:"DD")
+        }
+    }
+  }
+}
+</static-query>
+
+
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Hello, world!"
   }
-}
+};
 </script>
 
 <style>
