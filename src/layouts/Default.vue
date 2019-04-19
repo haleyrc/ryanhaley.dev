9f3bffd324d202607cf070871dd30c2c204d74b7
Ryan Haley <ryanhaley@frazer.biz>
2019-04-18 22:23:39 -0400

    Don't use g-link for rss feed

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -7,7 +7,7 @@
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about">About</g-link>
-        <g-link class="nav__link" to="/rss.xml">RSS</g-link>
+        <a class="nav__link" href="/rss.xml">RSS</a>
       </nav>
     </header>
     <slot/>
