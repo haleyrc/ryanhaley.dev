f668da43e9861c2fc76e44c8fce1c2d2b21e80cd
Ryan Haley <ryanhaley@frazer.biz>
2019-04-18 22:21:01 -0400

    Add RSS generation and link

--- gridsome.config.js
+++ gridsome.config.js
@@ -14,6 +14,35 @@ module.exports = {
         typeName: "Post",
         route: "/posts/:year/:month/:day/:slug"
       }
+    },
+    {
+      use: "gridsome-plugin-rss",
+      options: {
+        contentTypeName: "Post",
+        feedOptions: {
+          title: "ryanhaley.dev",
+          feed_url: "https://ryanhaley.dev/rss.xml",
+          site_url: "https://ryanhaley.dev"
+        },
+        feedItemOptions: (node) => {
+          const dt = new Date(node.date)
+          const year = dt.getFullYear()
+          const month = `${dt.getMonth() < 10 ? "0" : ""}${dt.getMonth()}`
+          const day = `${dt.getDate() < 10 ? "0" : ""}${dt.getDate()}`
+          return {
+            title: node.title,
+            description: node.description,
+            url: `https://ryanhaley.dev/posts/${year}/${month}/${day}/${
+              node.slug
+            }`,
+            author: "Ryan Haley"
+          }
+        },
+        output: {
+          dir: "./static",
+          name: "rss.xml"
+        }
+      }
     }
   ],
 
